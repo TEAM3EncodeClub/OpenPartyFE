db70672e0d0f72ff67d0d192a060d18bf560c442
David E. Perez Negron R <david@nethunters.xyz>
2023-04-03 17:24:10 -0600

    Just adding base code project

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ethers } from 'ethers'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  blockNumber: number | string | undefined;
+  provider: ethers.providers.BaseProvider;
+
+  constructor() {
+    this.provider = ethers.getDefaultProvider('goerli');
+  }
+
+  syncBlock(){
+    this.blockNumber = "loading..."
+    this.provider.getBlock('latest').then((block) => {
+      this.blockNumber = block.number;
+    })
+  }
+
+  clearBlock() {
+    this.blockNumber = 0;
+  }
+
+}
